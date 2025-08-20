acc1fe90d8cdc59197ea4c61a7d1c736173d1698
Atilla1907 <atilla.gorgulu@chasacademy.se>
2025-08-20 15:26:43 +0200

    added db.json file, added a few variables and created function to read/write

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
-const express =require("express");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
 const app = express();
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 8080;
+
+//function to help read/write the database
+function LoadmyData() {
+    return JSON.parse(fs.readFileSync("database.json", "utf8"));
+}
+
+function SavemyData(data) {
+    fs.writeFileSync("database.json", JSON.stringify(data, null, 2));
+}
